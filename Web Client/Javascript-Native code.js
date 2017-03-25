4fb65255c4bb5396f5b5b39cbe2d1f624d34f0cd
nguyenld <nguyenld1995@gmail.com>
2017-03-25 09:26:50 +0700

    Create Javascript-Native code.js

---
+++ Web Client/Javascript-Native code.js
@@ -0,0 +1,16 @@
+# root-me
+# Javascript - Native code
+# used javascript-deobfuscator add-on firefox.
+
+#result
+
+a = prompt('Entrez le mot de passe');
+if (a == 'toto123lol') {
+  alert('bravo');
+} else {
+  alert('fail...');
+}
+
+![](http://i.imgur.com/jmfhvGS.png)
+![](http://i.imgur.com/ezZM10c.png)
+![](http://i.imgur.com/55IPvTH.png)
